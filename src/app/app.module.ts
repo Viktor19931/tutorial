5ba79848e47c87ed393916c252f841cfe17564cf
Viktor Hordubey <viktor_hordubey@yahoo.com>
2018-04-18 23:47:22 +0300

    last changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CommentsComponent } from './comments/comments.component';
+import { ItemsComponent } from './items/items.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SharedService } from './shared/shared.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentsComponent,
+    ItemsComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
